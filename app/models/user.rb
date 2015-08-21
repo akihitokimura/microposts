1f3da5cb8cb61680cb16a0c3f3a439dd29f15c33
akihitokimura <a09021129102@gmail.com>
2015-08-21 10:04:11 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,22 @@ class User < ActiveRecord::Base
                format: { with: VALID_EMAIL_REGEX },
                uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0 } 
+    validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil:true } 
     has_many :microposts
+    has_many :following_relationships, class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
+    has_many :following_users, through: :following_relationships, source: :followed
+    def follow(other_user)
+    following_relationships.create(followed_id: other_user.id)
+    end
+
+
+    def unfollow(other_user)
+    following_relationships.find_by(followed_id: other_user.id).destroy
+    end
+    
+    def following?(other_user)
+    following_users.include?(other_user)
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Microposts
       g.helper false
       g.test_framework false
     end
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
